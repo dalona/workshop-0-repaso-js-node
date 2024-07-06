7a90103d6627da63c898d6f47fdffcf48b0bc48f
Daniel Alejandro Lopez Naranjo <dalona126@hotmail.com>
2024-07-05 20:27:41 -0500

    bullet 4 done

---
+++ API.Platzi/app.js
@@ -0,0 +1,26 @@
+document.getElementById('fetch-products').addEventListener('click',() =>{
+    fetchProducts();
+})
+
+
+const fetchProducts = async () => {
+    const response = await fetch ('https://api.escuelajs.co/api/v1/products')
+    const data = await response.json();
+    console.log(data)
+
+    const container = document.getElementById('products-list')
+
+    data.forEach(product => {
+        container.innerHTML += /*html*/ `
+            <div>
+                <h1>${product.title}</h1>
+                <p>${product.description}</p>
+                <img src=${product.images} alt="images">
+                <h3>$${product.price}.00</h3>
+            </div>
+        `
+    });
+};
+
+
+
